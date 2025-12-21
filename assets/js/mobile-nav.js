dd374f01c45f5f20df3722500f701b8be9585813
Melvin García <neodarknsf@gmail.com>
2025-12-21 14:37:36 -0600

    Improments navbar

--- assets/js/mobile-nav.js
+++ assets/js/mobile-nav.js
@@ -1,13 +1,15 @@
 (() => {
   const navToggle = document.querySelector(".nav-toggle");
-  if (!navToggle) return;
+  const nav = document.querySelector(".nav-menu");
 
-  const nav = document.querySelector(".nav-links");
+  if (!navToggle || !nav) return;
+
   const links = nav.querySelectorAll("a");
   const icon = navToggle.querySelector("i");
 
   navToggle.addEventListener("click", () => {
     nav.classList.toggle("open");
+
     icon.className = nav.classList.contains("open")
       ? "fa-solid fa-xmark"
       : "fa-solid fa-bars";
