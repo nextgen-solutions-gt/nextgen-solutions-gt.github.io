31e319155cfbda3d4df760e63ee8ba79a6684e86
Melvin García <neodarknsf@gmail.com>
2025-12-20 21:30:54 -0600

    Fixed redeclaration on mobile navbar

--- assets/js/mobile-nav.js
+++ assets/js/mobile-nav.js
@@ -2,6 +2,17 @@ const toggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav-links");
 const links = nav.querySelectorAll("a");
 
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
+
+// cerrar al hacer click
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("open");
+  });
+});
+
 const icon = toggle.querySelector("i");
 
 toggle.addEventListener("click", () => {
@@ -10,6 +21,3 @@ toggle.addEventListener("click", () => {
     ? "fa-solid fa-xmark"
     : "fa-solid fa-bars";
 });
-
-
-
