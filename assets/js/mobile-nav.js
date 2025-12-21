2fe8e1282702d7f797e5e8df837e5f008f7a6e64
Melvin García <neodarknsf@gmail.com>
2025-12-21 15:55:29 -0600

    Add more improvements

--- assets/js/mobile-nav.js
+++ assets/js/mobile-nav.js
@@ -1,22 +1,6 @@
-(() => {
-  const navToggle = document.querySelector(".nav-toggle");
-  if (!navToggle) return;
+const navToggle = document.getElementById("navToggle");
+const navMenu = document.getElementById("navMenu");
 
-  const nav = document.querySelector(".nav-links");
-  const links = nav.querySelectorAll("a");
-  const icon = navToggle.querySelector("i");
-
-  navToggle.addEventListener("click", () => {
-    nav.classList.toggle("open");
-    icon.className = nav.classList.contains("open")
-      ? "fa-solid fa-xmark"
-      : "fa-solid fa-bars";
-  });
-
-  links.forEach(link => {
-    link.addEventListener("click", () => {
-      nav.classList.remove("open");
-      icon.className = "fa-solid fa-bars";
-    });
-  });
-})();
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
