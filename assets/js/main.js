5d5a7c70a5d8baadc6ed7ce8bf7365c84661f1f1
Melvin García <neodarknsf@gmail.com>
2025-12-24 11:18:26 -0600

    Added more functions to nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,3 +85,14 @@ reveals.forEach(el => observer.observe(el));
     });
   });
 })();
+
+(() => {
+  const header = document.querySelector(".site-header");
+  if (!header) return;
+
+  const onScroll = () => {
+    header.classList.toggle("scrolled", window.scrollY > 10);
+  };
+
+  window.addEventListener("scroll", onScroll);
+})();
